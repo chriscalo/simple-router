3560dc438a5bd8610a2450f339c3c0a7bc0214f1
Chris Calo <chris.calo@gmail.com>
2018-05-01 13:19:08 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+// Router index file.
+// Convenience module so instead of
+//
+//   `import Router from "router/router"`
+//
+// we can just do
+//
+//   `import Router from "router"`
+//
+
+import Router from "./router";
+
+export default Router;
